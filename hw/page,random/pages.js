391f14ad1709195da37ac0d7cadfa2a571aef76f
Lee, Seungbeom <vldhtmxk@naver.com>
2020-03-30 16:57:25 +0900

    page,random commit

---
+++ hw/page,random/pages.js
@@ -0,0 +1,4 @@
+let studentCount = 31;
+let pageSize = 10;
+let page = studentCount/pageSize;
+console.log(Math.floor(page)+"장");
---
+++ hw/page,random/random.js
@@ -0,0 +1,4 @@
+for(let i= 0 ;i<30;++i){
+    let a = Math.floor(Math.random()*20+10);
+    console.log(a);
+}
