b1a12099745aa91382e9486958901ffac83c9d76
Lee, Seungbeom <vldhtmxk1@gmail.com>
2020-05-04 10:41:33 +0900

    시험

---
+++ hw/시험/mid5.js
@@ -0,0 +1,18 @@
+let a = [];
+
+for(let i=0;i<100; ++i) {
+a[i] = Math.floor(Math.random()*100+1);
+}
+for(let i = 0 ; i < a.length ;) {
+    if(a[i]%2 != 0) 
+        a.splice(i,1);
+    else
+        i++;
+        
+}
+a.sort(compareNumber);
+console.log(a);
+
+function compareNumber(i,j) {
+    return i-j;
+}
